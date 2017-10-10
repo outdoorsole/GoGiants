3540827e30eef4fd8d1ca160f536f19460afcd53
Maribel Montejano <outdoorsole@users.noreply.github.com>
2017-10-09 22:22:03 -0700

    Override 4 methods in VCGo&Giants classes; output viewWillAppear count

--- GoGiants/ViewControllerGiants.swift
+++ GoGiants/ViewControllerGiants.swift
@@ -20,6 +20,34 @@ class ViewControllerGiants: UIViewController {
         dismiss(animated: true, completion: nil)
     }
     
+    // Step 1: Override Methods
+    override func viewWillAppear(_ animated: Bool) {
+        // Call the superclass function it is overriding
+        super.viewWillAppear(animated)
+        // Print out a debug message
+        print("Giants: viewWillAppear")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        // Call the superclass function it is overriding
+        super.viewDidAppear(animated)
+        // Print out a debug message
+        print("Giants: viewDidAppear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        // Call the superclass function it is overriding
+        super.viewWillDisappear(animated)
+        // Print out a debug message
+        print("Giants: viewWillDisappear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        // Call the superclass function it is overriding
+        super.viewDidDisappear(animated)
+        // Print out a debug message
+        print("Giants: viewDidDissappear")
+    }
     
 }
 
--- GoGiants/ViewControllerGo.swift
+++ GoGiants/ViewControllerGo.swift
@@ -10,10 +10,44 @@
 import UIKit
 
 class ViewControllerGo: UIViewController {
+    // Step 2: Count how many times Go view controller appears
+    // Add a property count and initialize to zero.
+    var count: Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
+    // Step 1: Override Methods
+    override func viewWillAppear(_ animated: Bool) {
+        // Call the superclass function it is overriding
+        super.viewWillAppear(animated)
+        // Print out a debug message
+        print("Go: viewWillAppear")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        // Call the superclass function it is overriding
+        super.viewDidAppear(animated)
+        // Print out a debug message
+        print("Go: viewDidAppear")
+        // Increment the count property
+        count += 1
+        // Output the current count to the debug area.
+        print("current count: \(count)")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        // Call the superclass function it is overriding
+        super.viewWillDisappear(animated)
+        // Print out a debug message
+        print("Go: viewWillDisappear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        // Call the superclass function it is overriding
+        super.viewDidDisappear(animated)
+        // Print out a debug message
+        print("Go: viewDidDissappear")
+    }
 }
-
